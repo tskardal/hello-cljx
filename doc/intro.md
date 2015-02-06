e8042c5ff5c0d10b12cc0d845766f959f53cdd7b
Thomas Skardal <thomas.skardal@gmail.com>
2015-02-06 12:04:12 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hello-cljx
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hello_cljx/core_test.clj
@@ -0,0 +1,7 @@
+(ns hello-cljx.core-test
+  (:require [clojure.test :refer :all]
+            [hello-cljx.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
